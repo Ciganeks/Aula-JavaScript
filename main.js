80f1e5f83ece0dc7dba5a87a222d78e46981e940
Ciganeks <yago-gomez-costa@hotmail.com>
2024-05-09 00:10:14 -0300

    Adicionar arquivos da tarefa

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById("meuFormulario").addEventListener("submit", function(event){
+    event.preventDefault();  // Previne o comportamento padrão de envio do formulário
+
+    const campoA = parseFloat(document.getElementById("campoA").value);
+    const campoB = parseFloat(document.getElementById("campoB").value);
+    const resultadoDiv = document.getElementById("mensagemResultado");
+
+    if (campoB > campoA) {
+        resultadoDiv.textContent = "Formulário válido: Campo B é maior que Campo A.";
+        resultadoDiv.style.color = "green";
+    } else {
+        resultadoDiv.textContent = "Formulário inválido: Campo B deve ser maior que Campo A.";
+        resultadoDiv.style.color = "red";
+    }
+});
